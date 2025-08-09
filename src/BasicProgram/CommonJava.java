6ad1bce535b2888aa128aa761ad1429f28f2c501
My-Codes-21 <mdsarfaraaz3136@gmail.com>
2025-08-09 19:25:57 +0530

    first commit

--- src/BasicProgram/CommonJava.java
+++ src/BasicProgram/CommonJava.java
@@ -1,4 +1,17 @@
 package BasicProgram;
 
 public class CommonJava {
+    public static void main(String[] args) {
+        int age = 25;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        String name = "John";
+
+        System.out.println("age : "+age);
+        System.out.println("price : "+price);
+        System.out.println("grade : "+grade);
+        System.out.println("isJavaFun : "+isJavaFun);
+        System.out.println("name : "+name);
+    }
 }
